4a2753e1be645174821bf4325efb2bf5ce65f755
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-11-19 22:17:21 +0100

    ...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Hero } from "./";
+import { Hero, LiveRate } from "./";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <LiveRate />
     </>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as Countries } from "./Countries";
 export { default as Hero } from "./Hero";
+export { default as LiveRate } from "./LiveRate";
+export { default as CountrySort } from "./CountrySort";
 
 // This is a draft imports to practice MUI5
 export { default as Draft } from "./Draft";
